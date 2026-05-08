2d86a7a71d4db772613a62c167e84691c9b8e51f
astha panchal <asthapanchal@gmail.com>
2026-05-08 17:35:16 +0530

    fix api url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000'
+  baseURL: 'https://team-task-manager-production-ba80.up.railway.app/'
 })
 
 api.interceptors.request.use(config => {
